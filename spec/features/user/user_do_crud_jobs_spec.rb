6447e8745e92f26321bf3159002856ba7b6d5239
Allan Klaus Do Nascimento Bezerra <allan_klaus@globo.com>
2016-01-21 19:09:26 -0800

    Created test to check crud jobs

---
+++ spec/features/user/user_do_crud_jobs_spec.rb
@@ -0,0 +1,74 @@
+require 'rails_helper'
+
+feature 'User do CRUD on jobs' do
+  scenario 'user read jobs' do
+
+    5.times do |job|
+      create_job(nil, nil, job)
+    end
+
+    visit jobs_path
+
+    expect(page).to have_content "Macro Job 0"
+    expect(page).to have_content "Macro Job 1"
+    expect(page).to have_content "Macro Job 2"
+    expect(page).to have_content "Macro Job 3"
+    expect(page).to have_content "Macro Job 4"
+  end
+
+  scenario 'user create jobs' do
+    category = create_category
+    company = create_company
+    job = create_job
+
+    visit new_job_path
+
+    fill_in 'job[title]', with: job.title
+    fill_in 'job[location]', with: job.location
+    fill_in 'job[description]', with: job.description
+    select company.name, from: 'job|company_id|'
+    select category.name, from: 'job|category_id|'
+
+
+    click_on 'submit'
+
+    expect(page).to have_content job.title
+    expect(page).to have_content job.location
+    expect(page).to have_content job.description
+    expect(page).to have_content job.company.name
+    expect(page).to have_content job.category.name
+  end
+
+  scenario 'user update job' do
+    category = create_category
+    company = create_company
+    job = create_job(company, category)
+
+    visit edit_job_path(job)
+
+    # puts page.html
+
+    fill_in 'job[title]', with: job.title
+    fill_in 'job[location]', with: job.location
+    fill_in 'job[description]', with: job.description
+    select company.name, from: 'job|company_id|'
+    select category.name, from: 'job|category_id|'
+
+    click_on 'submit'
+
+    expect(page).to have_content job.title
+    expect(page).to have_content job.location
+    expect(page).to have_content job.description
+    expect(page).to have_content job.company.name
+    expect(page).to have_content job.category.name
+  end
+
+  scenario 'user try to create an invalid job' do
+    visit new_job_path
+
+    click_on 'submit'
+
+    expect(page).to have_content "Warning: All fields are necessary"
+  end
+
+end
--- spec/support/model_macros.rb
+++ spec/support/model_macros.rb
@@ -1,20 +1,20 @@
 module ModelsMacros
-  def create_company
-    Company.create(name: 'Campus Code',
-                   location: 'São Paulo',
+  def create_company(number = nil)
+    Company.create(name: "Creating Company #{number}",
+                   location: 'Guarujá',
                    phone: '11 2369 3476',
-                   mail: 'contato@campuscode.com.br')
+                   mail: "created#{number}@mail.com")
   end
 
-  def create_category
-    Category.create(name: 'Desenvolvedor')
+  def create_category(number = nil)
+    Category.create(name: "Category #{number}")
   end
 
-  def create_job(company = nil, category = nil)
-    company ||= create_company
-    category ||= create_category
-    company.jobs.create(title: 'Desenvolvedor Rails',
-               description: 'Desenvolvedor Full Stack Rails',
+  def create_job(company = nil, category = nil, number = nil)
+    company ||= create_company(number)
+    category ||= create_category(number)
+    company.jobs.create(title: "Macro Job #{number}",
+               description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                location: 'São Paulo - SP',
                category: category)
   end
