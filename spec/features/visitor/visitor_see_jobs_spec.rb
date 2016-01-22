88dad456671141d68ad6881926b5d01ba2208a4a
Allan Klaus Do Nascimento Bezerra <allan_klaus@globo.com>
2016-01-22 05:36:36 -0800

    Created test to see jobs, Beginning to apply css

---
+++ spec/features/visitor/visitor_see_jobs_spec.rb
@@ -0,0 +1,55 @@
+require 'rails_helper'
+
+feature 'Visitor visits home' do
+  scenario 'see jobs on home' do
+    5.times do |number|
+      create_job(nil, nil, number)
+    end
+
+    visit root_path
+
+    5.times do |number|
+      expect(page).to have_content "Macro Job #{number}"
+    end
+  end
+
+  scenario 'see job page' do
+    job = create_job(nil, nil, 10)
+
+    visit job_path(job)
+
+    expect(page).to have_content job.title
+    expect(page).to have_content job.location
+    expect(page).to have_content job.category.name
+    expect(page).to have_content job.company.name
+    expect(page).to have_content job.description
+  end
+
+  scenario 'see job on company page' do
+    company = create_company(5)
+    job = create_job(company, nil, 10)
+
+    visit company_path(company)
+
+    expect(page).to have_content company.name
+    expect(page).to have_content company.location
+
+    expect(page).to have_content job.title
+    expect(page).to have_content job.category.name
+    expect(page).to have_content job.location
+  end
+
+  scenario 'see job on category page' do
+    category = create_company(5)
+    job = create_job(nil, category, 10)
+
+    visit category_path(category)
+
+    expect(page).to have_content category.name
+
+    expect(page).to have_content job.title
+    expect(page).to have_content job.company.name
+    expect(page).to have_content job.location
+  end
+
+end
