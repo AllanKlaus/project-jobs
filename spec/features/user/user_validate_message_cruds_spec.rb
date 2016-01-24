47b3b323d2413f6ef319a897bfd6cce47f841306
Allan Klaus Do Nascimento Bezerra <allan_klaus@globo.com>
2016-01-24 05:58:03 -0800

    Validations on CRUD are done

--- spec/features/user/user_validate_message_cruds_spec.rb
+++ spec/features/user/user_validate_message_cruds_spec.rb
@@ -7,7 +7,7 @@ feature 'Validate all cruds' do
     visit new_job_path
     click_on 'submit'
 
-    expect(page).to have_content "Warning! All fields are mandatory."
+    expect(page).to have_content "Warning: All fields are necessary"
   end
 
   scenario 'company' do
@@ -16,7 +16,7 @@ feature 'Validate all cruds' do
     visit new_company_path
     click_on 'submit'
 
-    expect(page).to have_content "Warning! All fields are mandatory."
+    expect(page).to have_content "Warning: All fields are necessary"
   end
 
   scenario 'invalid category' do
@@ -25,6 +25,6 @@ feature 'Validate all cruds' do
     visit new_category_path
     click_on 'submit'
 
-    expect(page).to have_content "Warning! All fields are mandatory."
+    expect(page).to have_content "Warning: All fields are necessary"
   end
 end
