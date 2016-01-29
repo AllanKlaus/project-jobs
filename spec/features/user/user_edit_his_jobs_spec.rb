364ba22b7f413ef19826d1eb804251a2ca0410fc
Allan Klaus Do Nascimento Bezerra <allan_klaus@globo.com>
2016-01-29 05:56:10 -0800

    Created tests to user only edit his jobs

---
+++ spec/features/user/user_edit_his_jobs_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+feature 'User do CRUD on jobs' do
+  scenario 'user update his job' do
+    user = login_user
+
+    company = create_company(user: user)
+
+    company = create_company
+    job = create_job(company: company)
+
+    visit edit_job_path(job)
+
+    expect(page).to have_content 'Edit Job'
+
+    fill_in 'job[title]',       with: job.title
+    fill_in 'job[location]',    with: job.location
+    fill_in 'job[description]', with: job.description
+    select company.name,        from: 'job[company_id]'
+    select job.category.name,   from: 'job[category_id]'
+    select job.contract,        from: 'job[contract]'
+
+    click_on 'submit'
+
+    expect(page).to have_content job.title
+    expect(page).to have_content job.location
+    expect(page).to have_content job.description
+    expect(page).to have_content job.company.name
+    expect(page).to have_content job.category.name
+    expect(page).to have_content job.contract
+  end
+
+  scenario 'user try to update a job that dont belongs to him' do
+    user = login_user
+
+    company = create_company(user: user)
+
+    company = create_company
+    job = create_job(company: company)
+
+    visit edit_job_path(job)
+
+    expect(page).to have_content 'Warning: You can only edit your jobs'
+  end
+
+end
